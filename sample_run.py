45cd32f4c207c198e70b05c1d47576bbdd33d529
dkclauson003 <dkclauson003@outlook.com>
2020-01-05 19:41:17 +0800

    run.py

--- sample_run.py
+++ sample_run.py
@@ -1,66 +1,105 @@
-import sys
-import os
-from pathlib import Path
+#!/usr/bin/env python3
+
+import pathlib
 import platform
+import sys
+import subprocess
+import os
+import json
 
-# set HOME=%HOMEDRIVE%\%HOMEPATH%
-# set COURSE=NTNU Simultaneous Localization and Mapping
-# set EXAMTITLE=%COURSE% Final Exam 2016
-# set ROOTDIR=%HOME%\OneDrive\Documents\%COURSE%\%EXAMTITLE%
-# set EXAMDIR=%HOME%\OneDrive\Documents\Examinations
-# set TEMPLATE=%EXAMDIR%\Templates
-# set CODEDIR=%ROOTDIR%\Code
+GIT_CMD="git"
 
-COURSE = 'Machine Learning'
-YEAR = 2019
-EXAMTYPE = 'Final Exam'
-UNIVERSITY = 'NTNU'
+class cd:
+    """Context manager for changing the current working directory"""
+    def __init__(self, newPath):
+        self.newPath = pathlib.Path(newPath).expanduser().resolve()
 
-EXAMTITLE = COURSE + ' - ' + EXAMTYPE  + ' - ' + UNIVERSITY + ' - ' + str(YEAR)
+    def __enter__(self):
+        self.savedPath = pathlib.Path.cwd()
+        os.chdir(self.newPath)
 
-host = os.getenv('HOSTNAME', os.getenv('COMPUTERNAME', platform.node())).split('.')[0]
+    def __exit__(self, etype, value, traceback):
+        os.chdir(self.savedPath)
 
-ROOTDIR = Path( paths['ROOTDIR'] )
-EXAMDIR = Path( paths['EXAMDIR'] )
+def updateGit( url, branch,  root ):
+    dirStart = url.rfind("/")    
+    dirname=url[dirStart+1:-4]
 
-if ( ROOTDIR is None ) or ( EXAMDIR is None ):
-    print("ERROR: Unable to find ROOTDIR", ROOTDIR, "or EXAMDIR", EXAMDIR)
-    sys.exit(2)
+    with cd( root ):
+        p = pathlib.Path( dirname )
+        if ( branch ):
+            bs = " --branch " + branch
+        else:
+            bs = ""
+        if not p.is_dir():
+            print("cloning {0} from url {1} root {2}".format( dirname, url, root ), 'git command', GIT_CMD)
+                
+            cmd = GIT_CMD + " clone " + bs + " " + url + " " + dirname 
+            os.system( cmd )
+        else:
+            print("git directory exists")
 
-TEMPLATEDIR = EXAMDIR  / 'Templates' 
+        with cd( dirname ):
+            print("Executing git pull")
+            o = None
+            try:
+                o = subprocess.check_output(GIT_CMD + " pull", shell=True)
+            except subprocess.CalledProcessError:
+                pass
+            if ( o ):
+                print( 'git pull:' + o.decode('utf-8') )
+
+f = pathlib.Path(__file__)
+d = f.parent.resolve()
+
+COURSE, EXAMTYPE, UNIVERSITY, YEAR = d.name.split(' - ')
+
+HOME = pathlib.Path.home()
+
+EXAMTITLE = " - ".join( [ COURSE, EXAMTYPE, UNIVERSITY, YEAR ] )
+
+ROOTDIR = pathlib.Path(".")
+updateGit( "http://www.github.com/mrkwss007/examinations.git", "master", ROOTDIR )
+EXAMDIR = ROOTDIR / "examinations"
+
+TEMPLATE = EXAMDIR / 'Templates'
 CODEDIR = ROOTDIR / 'Code'
-STYLES = TEMPLATEDIR / 'exam_template.css'
 
-codePaths = [ Path('.' + '/' + 'Code' ).resolve(),
-              CODEDIR,
-              CODEDIR / '..' / '..' /  'Code' ]
+print('EXAMDIR', EXAMDIR, 'EXAMTITLE', EXAMTITLE, 'TEMPLATE', TEMPLATE)
 
-questionPaths = [ Path('.' + '/' + 'Questions' ).resolve(),
-                  ROOTDIR / 'Questions',
-                  ROOTDIR  / '..' / 'Questions' ]
+PARENT = pathlib.Path("..")
 
-#for k in os.environ:
-#    print('k', k, '=', os.environ[k] )
+values = {
+    'university': UNIVERSITY,
+    'coursetitle': COURSE,
+    'examtype': EXAMTYPE,
+    'examiners': "包傑奇",  # "Jacky Baltes",
+    'date': "Monday, 6th January 2020",
+    'time': "9:30 - 12:30",
+    'room': "NTNU TA412",
+    'note': """
+Attempt all questions.<br>
+Show your work to receive full marks.<br>
+This is an <b>open book/open internet</b> examination. You are allowed to use any printed material, your computer, and the Internet.<br>
+You are <b>not</b> allowed to use any form of communication including talking to your class mates, mobile phones, Facebook, email, instant messenger, and similar systems.<br>
+Use of communication in any form will lead to <b>immediate</b> disqualification.<br>
+Some of the questions may not be solvable, that is it may be impossible to calculate the requested information. In this case, say so in your answer and explain why.<br>
+"""
+}
 
-PYTHON_EXECUTABLE = sys.executable
-VERBOSE = '-v -v'
+cmd = [ sys.executable,
+        str(EXAMDIR / 'create_exam.py'),
+        '-v',
+        '-v',
+        str(EXAMTITLE) + '.py',
+        '--template_dir', str(TEMPLATE),
+        '--resource_dir', str(ROOTDIR / 'Code'), 
+        '--resource_dir', str(PARENT / 'Code'), 
+        '--resource_dir', str(ROOTDIR / 'Questions'), 
+        '--resource_dir', str(PARENT / 'Questions'), 
+        '--styles', str( TEMPLATE / 'exam_template.css' ),
+        '--values', json.dumps( values )
+]
 
-examPath = Path('.' + '/' + EXAMTITLE + ".py").resolve()
-print('+++ examPath', examPath )
-
-commandLine = PYTHON_EXECUTABLE \
-                + ' ' '"' + str( EXAMDIR / 'create_exam.py' ) + '"' \
-                + ' ' + VERBOSE + ' ' \
-                + ' ' + '"' + str( examPath ) + '"' \
-                + ' -t ' + '"' + str(TEMPLATEDIR) + '"' \
-                + ' -s ' + '"' + str(STYLES) + '"'
-
-for q in questionPaths:
-    print('q',q)
-    commandLine = commandLine + ' -r ' + '"' + str(q) + '"' + ' '
-
-for c in codePaths:
-    commandLine = commandLine + ' -r ' + '"' + str(c) + '"' + ' '
-
-print('Command line', commandLine )
-os.system( commandLine )
+print('Executing command', cmd)
+subprocess.call( cmd )
